8b54612a5fc5eae5ef6675255dda1ffcb84b1a67
YUNa <jahitosis1@up.edu.ph>
2025-07-12 09:53:58 +0800

    edits

--- multiplayer_spawner.gd
+++ multiplayer_spawner.gd
@@ -6,18 +6,19 @@ var players = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#spawn_function = spawn_player
+	spawn_function = spawn_player
 	if is_multiplayer_authority():
-		multiplayer.peer_connected.connect(spawn_player)
+		spawn(1)
+		multiplayer.peer_connected.connect(spawn)
 		multiplayer.peer_disconnected.connect(remove_player)
-		spawn_player()
 
 func spawn_player(data = 1):
 	var p = player.instantiate()
 	p.set_multiplayer_authority(data)
 	players[data] = p
 	p.name = str(data)
-	call_deferred("add_child", p)
+	#call_deferred("add_child", p)
+	return p
 
 func remove_player(data):
 	players[data].queue_free()
